41d6c54b15cf4bbc426ddae6e1041ccb661250ae
Moises Velasquez <mvelasquezdeveloper@gmail.com>
2023-03-16 12:47:47 -0500

    accumulate app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'angular-basic-h';
+  title = 'Counter app';
+  count = 0;
+
+  accumulate(v: number) {
+    this.count += v;
+  }
 }

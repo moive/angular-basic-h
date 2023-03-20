e786b4c85eae5f556b65c67f7ffdb3ca0b8cda45
Moises Velasquez <mvelasquezdeveloper@gmail.com>
2023-03-20 12:26:47 -0500

    first way with input

--- src/app/dbz/main-page/main-page.component.ts
+++ src/app/dbz/main-page/main-page.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
 
+interface Character {
+  name: string;
+  power: number;
+}
+
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
   styleUrls: ['./main-page.component.scss'],
 })
 export class MainPageComponent {
+  newCharacter: Character = {
+    name: 'Trunck',
+    power: 14000,
+  };
+
   add() {
-    console.log('hi');
+    console.log(this.newCharacter);
+  }
+
+  changeName(e: any) {
+    console.log(e.target.value);
   }
 }

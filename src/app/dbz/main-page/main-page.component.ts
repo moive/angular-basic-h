dd1e5ec62dda849fd5500d3b00371c8c5e04432e
Moises Velasquez <mvelasquezdeveloper@gmail.com>
2023-03-20 12:16:59 -0500

    add FormModule same behavior preventDefault

--- src/app/dbz/main-page/main-page.component.ts
+++ src/app/dbz/main-page/main-page.component.ts
@@ -3,8 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
-  styleUrls: ['./main-page.component.scss']
+  styleUrls: ['./main-page.component.scss'],
 })
 export class MainPageComponent {
-
+  add() {
+    console.log('hi');
+  }
 }
